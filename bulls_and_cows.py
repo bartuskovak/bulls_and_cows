61e9fd52bb25e154851682dc3df7e3ad8335676e
bartuskovak <kati.bartuskova@gmail.com>
2023-10-06 17:51:54 +0200

    verze2

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -9,46 +9,60 @@ from random import randint
 
 line = ("-"*50)
 
+def has_unique_digits(number) -> bool:
+    for element in number:
+        if number.count(element) > 1:
+            return False
+    return True
+
 def choose_number() -> str:
-"""
-description:
-----------
-Choose random four digit number
-between 1023 and 9876 and check,
-if no digit occurs twice in a number.
-"""
-     number = str(randint(1023,9876))
-     for element in number:
-         if number.count(element) > 1:
-           return choose_number()
-     return number
+    """
+    Choose random four digit number
+    between 1023 and 9876 and check,
+    if no digit occurs twice in a number.
+    """
+
+    number = str(randint(1023,9876))
+    if not has_unique_digits(number):
+        return choose_number()
+    return number
 
 def number_check() -> str:
-"""
-Let user enter a number and check,
-if it is four digit, every digit is 
-here only once, and it does not start
-with 0,
-"""
-    your_number = input("Enter a number:")
-    print(line)
-    if len(your_number) =!= 4:
-        raise ValueError ("Enter a four digit number.")
-    elif your_number[0] == "0":
-        raise ValueError ("Number cannot start with 0.")
-    elif number.count(element) > 1:
-        raise ValueError ("Use every digit only once")
-    elif your_number.isnumeric():
-        print(">>>", your_number)
-    else:
-        raise ValueError ("You can only use numbers.")
+    """
+    Let user enter a number and check,
+    if it is four digit, every digit is 
+    here only once, and it does not start
+    with 0,
+    """
+    
+    number_ok = False
+    while not number_ok:
+        your_number = input("Enter a number:")
+        if your_number == "exit":
+            exit(1)
+        if not your_number.isnumeric():
+            print("You can only use numbers.")
+            continue        
+        elif not has_unique_digits(your_number):
+            print("Use every digit only once")
+            continue
+        elif len(your_number) != 4:
+            print ("Enter a four digit number.")
+            continue
+        elif your_number[0] == "0":
+            print ("Number cannot start with 0.")
+            continue
+        
+        number_ok = True
+    
     return your_number
 
 def bull_counter(secret_number: str, number_guess: str) -> int:
-"""
-Count bulls (one bull for every correctly
-guessed digit).
-"""
+    """
+    Count bulls (one bull for every correctly
+    guessed digit).
+    """
+
     position = 0
     bulls = [0,0,0,0]
     for element in secret_number:
@@ -58,10 +72,11 @@ guessed digit).
     return bulls
     
 def cow_counter(secret_number: str, number_guess, bulls: str) -> int:
-"""
-Count cows (one cow for every correct digit,
-but in a wrong place).
-"""
+    """
+    Count cows (one cow for every correct digit,
+    but in a wrong place).
+    """
+
     position = 0
     cows = 0
     for element in secret_number:
@@ -71,11 +86,10 @@ but in a wrong place).
     return cows
 
 def game(secret_number: str, number_guess: str) -> str:
-"""
-Tells what is your score.
-"""
-    secret_number = choose_number()
-    number_guess = number_check()
+    """
+    Tells what is your score.
+    """
+
     attempts = 0
     while secret_number != number_guess:
         attempts += 1
@@ -107,6 +121,8 @@ if __name__ == "__main__":
     print("Let's play a bulls and cows game.")
     print(line)
 
+    secret_number = choose_number()
+    number_guess = number_check()
     game(secret_number, number_guess)
 
 
